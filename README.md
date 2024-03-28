f18ddec0d513f6f8b04a313eda8d335c8dbb9ab2
pawurb <contact@pawelurbanek.com>
2024-03-28 14:55:36 +0100

    Add time-based expiration

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # zcache [![Latest Version](https://img.shields.io/crates/v/zcache.svg)](https://crates.io/crates/zcache) [![GH Actions](https://github.com/pawurb/zcache/actions/workflows/ci.yml/badge.svg)](https://github.com/pawurb/zcache/actions)
 
-WIP
+Zcache is an in-memory cache store with time based expiration. The goal of this project is to provide the simplest possible API for in-memory caching.
+
+## Usage
+
+...
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use std::future::Future;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use thiserror::Error;
 
-static mut ZCACHE_STORE: Lazy<HashMap<String, Box<ZEntry>>> = Lazy::new(HashMap::new);
+static mut ZCACHE_STORE: Lazy<HashMap<String, (u128, Box<ZEntry>)>> = Lazy::new(HashMap::new);
 
 #[derive(Error, Debug)]
 #[non_exhaustive]
@@ -23,7 +24,11 @@ pub enum ZEntry {
 pub struct ZCache {}
 
 impl ZCache {
-    pub async fn fetch<F, Fut>(key: &str, f: F) -> Result<ZEntry, ZCacheError>
+    pub async fn fetch<F, Fut>(
+        key: &str,
+        expires_in: Option<Duration>,
+        f: F,
+    ) -> Result<ZEntry, ZCacheError>
     where
         F: FnOnce() -> Fut,
         Fut: Future<Output = Option<ZEntry>>,
@@ -32,7 +37,7 @@ impl ZCache {
             Some(value) => Ok(value),
             None => match f().await {
                 Some(value) => {
-                    Self::write(key, value.clone());
+                    Self::write(key, value.clone(), expires_in);
                     Ok(value)
                 }
                 None => Err(ZCacheError::FetchError(key.to_string())),
@@ -43,35 +48,69 @@ impl ZCache {
     pub fn read(key: &str) -> Option<ZEntry> {
         let key = key.to_string();
         let result = unsafe { ZCACHE_STORE.get(&key) };
-        result.map(|value| *value.clone())
+        match result {
+            Some((valid_until, value)) => {
+                let valid_until = *valid_until;
+                if valid_until == 0 || valid_until > now_in_millis() {
+                    Some(*value.clone())
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
     }
 
-    pub fn write(key: &str, value: ZEntry) {
+    pub fn write(key: &str, value: ZEntry, expires_in: Option<Duration>) {
         let key = key.to_string();
+
+        let valid_until: u128 = match expires_in {
+            Some(duration) => now_in_millis() + duration.as_millis(),
+            None => 0,
+        };
         unsafe {
-            ZCACHE_STORE.insert(key, Box::new(value));
+            ZCACHE_STORE.insert(key, (valid_until, Box::new(value)));
         }
     }
 }
 
+fn now_in_millis() -> u128 {
+    SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards!")
+        .as_millis()
+}
+
 #[cfg(test)]
 mod tests {
+    use std::ops::Mul;
+    use std::thread::sleep;
+
     use super::*;
 
     #[tokio::test]
     async fn read_write_works() {
         let cacheable = ZEntry::Int(1);
-        ZCache::write("one", cacheable);
-        let result = ZCache::read("one");
+        let one_second = Duration::from_secs(1);
+        ZCache::write("key1", cacheable, Some(one_second));
+        let result = ZCache::read("key1");
 
         match result {
             Some(ZEntry::Int(value)) => assert_eq!(value, 1),
             _ => panic!("Unexpected value"),
         }
 
+        sleep(one_second.mul(2));
+        let result = ZCache::read("key1");
+
+        if result.is_some() {
+            panic!("Entry should be expired!");
+        }
+
         let cacheable = ZEntry::Text("cached text".to_string());
-        ZCache::write("two", cacheable);
-        let result = ZCache::read("two");
+        ZCache::write("key2", cacheable, None);
+        sleep(one_second.mul(2));
+        let result = ZCache::read("key2");
         match result {
             Some(ZEntry::Text(value)) => assert_eq!(value, "cached text".to_string()),
             _ => panic!("Unexpected value"),
@@ -81,11 +120,32 @@ mod tests {
     #[tokio::test]
     async fn fetch_works() {
         let cacheable = ZEntry::Int(1);
-        let result = ZCache::fetch("one", || async { Some(cacheable.clone()) }).await;
+        let result = ZCache::fetch("key1", None, || async { Some(cacheable.clone()) }).await;
 
         match result {
             Ok(ZEntry::Int(value)) => assert_eq!(value, 1),
             _ => panic!("Unexpected value"),
+        }
+    }
+
+    #[tokio::test]
+    async fn fetch_expiry_works() {
+        let cacheable = ZEntry::Int(1);
+        let one_second = Duration::from_secs(1);
+        let result = ZCache::fetch("key1", Some(one_second), || async {
+            Some(cacheable.clone())
+        })
+        .await;
+        match result {
+            Ok(ZEntry::Int(value)) => assert_eq!(value, 1),
+            _ => panic!("Unexpected value"),
+        }
+
+        sleep(one_second.mul(2));
+        let result = ZCache::read("key1");
+
+        if result.is_some() {
+            panic!("Entry should be expired!");
         }
     }
 }
